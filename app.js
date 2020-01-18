0fd1435a14ce2f0a7a84c81277fed181bc40f9a8
Allison Harrison <allisonrharrison@gmail.com>
2020-01-17 16:17:49 -0800

    Express Session Part 2

--- app.js
+++ app.js
@@ -149,4 +149,27 @@ function auth(req, res, next) {
   }
 }
 
+app.use('/', index);
+app.use('/users', users);
+
+function auth (req, res, next) {
+    console.log(req.session);
+
+  if(!req.session.user) {
+      var err = new Error('You are not authenticated!');
+      err.status = 403;
+      return next(err);
+  }
+  else {
+    if (req.session.user === 'authenticated') {
+      next();
+    }
+    else {
+      var err = new Error('You are not authenticated!');
+      err.status = 403;
+      return next(err);
+    }
+  }
+}
+
 module.exports = app;
